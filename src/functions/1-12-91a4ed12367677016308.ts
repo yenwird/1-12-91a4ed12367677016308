74943170ff23c6b18a7feba7275dd736dde47a55
yenwird <147008695+yenwird@users.noreply.github.com>
2023-11-27 17:59:40 -0400

    Update 1-12-91a4ed12367677016308.ts

--- src/functions/1-12-91a4ed12367677016308.ts
+++ src/functions/1-12-91a4ed12367677016308.ts
@@ -3,14 +3,14 @@ import * as https from "https";
 import * as df from 'durable-functions';
 import { ActivityHandler, OrchestrationContext, OrchestrationHandler } from 'durable-functions';
 
-const testIdentity2 => {}
+const testIdentity2 => {};
 
 export async function serviceBusQueueTrigger(message: unknown, context: InvocationContext): Promise<void> {
     context.log('Service bus queue function processed message:', message);
     const client = df.getClient(context);
     const instanceId: string = await client.startNew("1-12-91a4ed12367677016308", message);
     context.log(`Started orchestration with ID = '${instanceId}'.`);
-}
+};
 app.serviceBusQueue('1-12-91a4ed12367677016308', {
     connection: 'azureQueueConnection',
     queueName: '1-12-91a4ed12367677016308',
